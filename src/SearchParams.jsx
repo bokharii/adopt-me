9fa43241c70e953fa692393566ce1690e59aae34
Saaquib Bokhari <saaquibhbokhari@gmail.com>
2023-05-07 17:48:51 -0400

    added API calls for pet names, breeds, and animal type

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,15 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./pet";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState(""); //hooks: start with "use"
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]); //pets to be retrieved from the API
   const breeds = [];
+
+  useEffect(() => {
+    //hook to retrieve data from outside of component (usually API)
+    requestPets();
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -53,6 +74,14 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          breed={pet.breed}
+          animal={pet.animal}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
